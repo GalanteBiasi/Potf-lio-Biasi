f9341ab0c79db5b3f3714db1d23858207d7e64dc
GalanteBiasi <galantebiasi@gmail.com>
2025-06-02 08:30:44 -0300

    Att

--- script.js
+++ script.js
@@ -5,3 +5,25 @@ const mobileMenu = document.getElementById('mobile-menu');
     mobileMenu.classList.toggle('hidden');
   });
 
+ const track = document.querySelector('.carousel-track');
+  const items = document.querySelectorAll('.carousel-item');
+  const prevBtn = document.getElementById('prev');
+  const nextBtn = document.getElementById('next');
+  let index = 0;
+
+  function updateCarousel() {
+    const width = items[0].clientWidth;
+    track.style.transform = `translateX(-${index * width}px)`;
+  }
+
+  prevBtn.addEventListener('click', () => {
+    index = (index === 0) ? items.length - 1 : index - 1;
+    updateCarousel();
+  });
+
+  nextBtn.addEventListener('click', () => {
+    index = (index + 1) % items.length;
+    updateCarousel();
+  });
+
+  window.addEventListener('resize', updateCarousel);
